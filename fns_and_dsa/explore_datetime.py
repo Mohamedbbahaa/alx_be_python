82cddd562624785a5df1d8d62449ee4f0a617d54
Mohamedbbahaa <mohamedbbahaa404@gmail.com>
2024-09-29 04:29:12 +0300

    .

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -2,7 +2,7 @@ import datetime
 from datetime
 
 def display_current_datetime():
-    current_date = datetime.datetime.now()
+    current_date = datetime.datetime.now(Y-m-d H:M:S)
     print(f"Current date and time: {current_date}")
 display_current_datetime()
 number_of_days = int(input("Enter the number of days to add to the current date: "))
