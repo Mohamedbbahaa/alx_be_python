218dee92941577d12d1597210dfce31607d09e38
Mohamedbbahaa <mohamedbbahaa404@gmail.com>
2024-10-09 23:26:09 +0300

    bank

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -10,6 +10,6 @@ class BankAccount:
         else:
             return False
     def display_balance(self):
-        print("Current Balance:" + self.account_balance)
+        print("Current Balance:"  f"${self.account_balance:.2f")
 
     
